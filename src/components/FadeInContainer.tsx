1449260296834a0418994833bc1669cd25bd2b39
firzhaardhia <firzharamadhan27@gmail.com>
2024-11-11 21:46:54 +0700

    clear comment

--- src/components/FadeInContainer.tsx
+++ src/components/FadeInContainer.tsx
@@ -10,22 +10,14 @@ const FadeInContainer = ({ children }: LayoutProps) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // Trigger the animation after the component mounts
     const timeoutId = setTimeout(() => {
       setIsVisible(true);
-    }, 100); // Delay for the transition effect
+    }, 100);
 
-    return () => clearTimeout(timeoutId); // Clean up timeout
+    return () => clearTimeout(timeoutId);
   }, []);
 
   return (
-    // <div
-    //   className={`transform transition-all duration-1000 ease-out z-0 ${
-    //     isVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full'
-    //   }`}
-    // >
-    //   {children}
-    // </div>
     <div className="relative overflow-hidden h-full w-full">
       <div
         className={`absolute inset-0 transform transition-all duration-1000 ease-out ${
