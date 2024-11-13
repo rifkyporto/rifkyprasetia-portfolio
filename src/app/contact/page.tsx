1a8897d56592a09dd3cf58248bad5e00b5e447fc
firzhaardhia <firzharamadhan27@gmail.com>
2024-11-13 16:50:11 +0700

    handle set show social on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
 import supabase from "@/utils/supabase";
 import Layout from "@/components/Layout";
-
+import { Icon } from "@iconify/react";
+import { SOCIALLIST } from "@/lib/configs";
 export const dynamic = 'force-static';
-
+// export const revalidate = 0
 const Contact = async () => {
   const { data: profile } = await supabase
     .from('profile')
@@ -11,7 +12,14 @@ const Contact = async () => {
       *
     `)
     .eq('id', process.env.NEXT_PUBLIC_SUPABASE_USER_ID)
-  console.log({profile})
+
+  const { data: socials } = await supabase
+    .from('social')
+    .select(`
+      *
+    `)
+    .eq('is_show_contact_page', true)
+  console.log({profile, socials})
   const profileData = profile?.[0];
 
   return (
@@ -23,7 +31,17 @@ const Contact = async () => {
             <div className="flex flex-col gap-1">
               <p>{profileData?.email}</p>
               <p>{profileData?.mobile}</p>
-              <p>@rifkyprasetia</p>
+              <div className="flex flex-col gap-2 my-1">
+                {socials?.map((social) => {
+                  return (
+                    <div className="flex gap-2 items-center">
+                      <Icon className="text-2xl" icon={SOCIALLIST?.find((socialItem) => socialItem?.socialKey === social?.key)?.logo!}/>
+                      <p>{social?.username}</p>
+                    </div>
+                  )
+                })}
+              </div>
+              {/* <p>@rifkyprasetia</p> */}
             </div>
           </div>
           <img
@@ -45,6 +63,13 @@ const Contact = async () => {
           <div className="w-full h-full flex flex-col justify-center mt-20 items-end gap-3 text-right sm:pr-[5rem] pr-5">
             <p className="text-4xl font-semibold">{profileData?.display_name}</p>
             <div className="flex flex-col gap-1 text-xl">
+              {/* {socials?.map((social) => {
+                return (
+                  <div>
+                  
+                  </div>
+                )
+              })} */}
               <p>@rifkyprasetya</p>
               <p>{profileData?.email}</p>
               <p>{profileData?.mobile}</p>
