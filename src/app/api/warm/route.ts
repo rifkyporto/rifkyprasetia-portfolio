805dca595b0668b07c485eb4b78e0ba1720d9e92
firzhaardhia <firzharamadhan27@gmail.com>
2024-11-15 09:31:01 +0700

    hadeu

--- src/app/api/warm/route.ts
+++ src/app/api/warm/route.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
-export default async function POST(
+export async function POST(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
